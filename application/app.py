87155ee4df0192b0f3e6f16ed987e3ea4ee512dc
ihor.kosovych@ronati.com <ihor.kosovych@ronati.com>
2021-04-07 18:53:28 +0300

    Changing main page.

--- application/app.py
+++ application/app.py
@@ -8,7 +8,13 @@ from ml_solution_final import prediction
 
 
 def main():
-	st.title("Prediction will student's pass or fail his or her exam!")
+	st.title("How toxic is your text?")
+	text = st.text_input("Input you comment here")
+	model_output = prediction.predict_probability(text)
+	result_in_percent = round(model_output, 3) * 100
+	if text:
+		st.write("Toxicty of you comment is: ", result_in_percent)
+	print(result_in_percent)
 
 
 if __name__ == "__main__":
